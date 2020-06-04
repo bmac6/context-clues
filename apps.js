ea4eb96011bb8cbd18654fc3ebc0c683aabf6fae
bmac6 <bryanamc@yahoo.com>
2020-06-03 21:31:16 -0500

    commited

---
+++ apps.js
@@ -0,0 +1,41 @@
+SetName();
+function SetName() {
+    
+    var name= 'Chester Copperpot';
+console.log(name);
+};
+
+console.log ('Some Text');
+let avg = findAvg(4, 7);
+console.log('Some Text', avg);
+function findAvg (a, b) {
+    console.log('Some Text');
+    var answer = (a+b) / 2;
+    return answer;
+}
+let fruits = ['pineapple', 'blueberry', 'strawberry'];
+
+function printFruits() {
+    let favFruit = fruits[2];
+    console.log(fruits[0]);
+   
+   
+    
+    function fruitFav() {
+        console.log(favFruit);
+    }
+    
+    console.log(leastFav)
+    fruitFav();
+
+}
+
+printFruits();
+
+let leastFav= fruits[0];
+
+callMe();
+function callMe () {
+    console.log('Hello', name)
+}
+let alertMe
